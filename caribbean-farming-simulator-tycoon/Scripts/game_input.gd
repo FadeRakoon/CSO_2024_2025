88164232cbf74f4e09518d3cfb351cb7f78a3f31
Jojo5673 <judoudo2005@gmail.com>
2024-12-01 00:06:42 -0500

    Omni-Directional movement
    Added movement in 8 directions instead of just 4

--- caribbean-farming-simulator-tycoon/Scripts/game_input.gd
+++ caribbean-farming-simulator-tycoon/Scripts/game_input.gd
@@ -4,15 +4,18 @@ static var direction: Vector2
 
 static func movement_input() -> Vector2:
 	if Input.is_action_pressed("walk_up"):
-		direction = Vector2.UP
+		direction.y = -1
 	elif Input.is_action_pressed("walk_down"):
-		direction = Vector2.DOWN
-	elif Input.is_action_pressed("walk_left"):
-		direction = Vector2.LEFT
-	elif Input.is_action_pressed("walk_right"):
-		direction = Vector2.RIGHT
+		direction.y = 1
 	else:
-		direction = Vector2.ZERO
+		direction.y = 0
+	
+	if Input.is_action_pressed("walk_left"):
+		direction.x = -1
+	elif Input.is_action_pressed("walk_right"):
+		direction.x = 1
+	else:
+		direction.x = 0
 	return direction
 #updates direction for the class and returns it if needed
 
