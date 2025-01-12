6c96fa2ffd7f27f6d7f7f4518b689740fd6b1d28
Bluwinters <39880735+Bluwinters@users.noreply.github.com>
2025-01-11 22:27:03 -0500

    Natural Disasters

--- Nothing Nuh Grow Suh - project files/Scenes/objects/plants.gd
+++ Nothing Nuh Grow Suh - project files/Scenes/objects/plants.gd
@@ -6,6 +6,9 @@ var can_grow : bool
 var growth_factor = 1
 var frame_count
 var grown: bool
+var circumstance : bool
+var fertility : int
+var moisture : int 
 
 func set_crop(plant_type : DataTypes.Plants):
 	
@@ -15,9 +18,10 @@ func set_crop(plant_type : DataTypes.Plants):
 		set_can_grow(true)
 		return
 	#File path for the plant resource "Resource Folder + Name of the Plant + Resource File Extension"
-	var resource_path = "res://Plant Resources/" + DataTypes.Plants.keys()[plant_type] + ".tres" 
+	var resource_path = "res://Resource Files/Plants/" + DataTypes.Plants.keys()[plant_type] + ".tres" 
 	#Enable growth, though this could be added to the resource to avoid the need to add if or match statements for debris or other non plants that use this resource
 	can_grow = true
+	circumstance = true
 	
 	#load and create a copy of the resource
 	plant_instance = load(resource_path).duplicate()
@@ -30,9 +34,10 @@ func set_crop(plant_type : DataTypes.Plants):
 	sprite.set_speed_scale(0)
 	frame_count = sprite.get_sprite_frames().get_frame_count(DataTypes.Plants.keys()[plant_type])
 	
-		
 	 
 func grow_plant(growth_mag : int):
+	
+	update_can_grow()
 	if can_grow and plant_instance:
 			var days_grown = plant_instance.get_days_grown()
 			plant_instance.set_days_grown(days_grown + growth_mag)
@@ -49,8 +54,9 @@ func harvest():
 func set_can_grow(value : bool):
 	can_grow = value
 	
-func check_can_grow(fert : int, moist : int):
-	pass
+func update_can_grow():
+	set_can_grow(fertility >= plant_instance.min_fertility and moisture >= plant_instance.min_moist and circumstance)
+	return
 	
 func _process(delta: float) -> void:
 	if plant_instance:
