668b16a88cdaa036a9a5b5c4cceb67d6e134c0d4
Jojo5673 <judoudo2005@gmail.com>
2025-01-14 14:40:37 -0500

    Revert "Merge branch 'main' into soil-mechanics-pt-2"
    This reverts commit 554c76d8b3dad438a25616d37d5958f0cb36fc53, reversing
    changes made to 9a241602567a5b3aa405ffb0fe774661d01b88c7.

--- Nothing Nuh Grow Suh - project files/Scenes/ui/game_menu_screen.gd
+++ Nothing Nuh Grow Suh - project files/Scenes/ui/game_menu_screen.gd
@@ -1,19 +1,19 @@
 extends CanvasLayer
 
 
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
 
 
 func _on_start_game_button_pressed() -> void:
-	GameManager.start_game()
-	queue_free()
+	pass # Replace with function body.
+
 
 func _on_exit_pressed() -> void:
-	GameManager.exit_game()
+	pass # Replace with function body.
--- Nothing Nuh Grow Suh - project files/Scripts/globals/game_manager.gd
+++ Nothing Nuh Grow Suh - project files/Scripts/globals/game_manager.gd
@@ -1,47 +1,8 @@
 extends Node
 
-var game_menu_screen = preload("res://Scenes/ui/game_menu_screen.tscn")
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("game_menu"):
-		show_game_menu_screen()
-
-func show_game_menu_screen() ->void:
-	var game_menu_screen_instance = game_menu_screen.instantiate()
-	get_tree().root.add_child(game_menu_screen_instance)
-
-
-var selected_map: DataTypes.map = DataTypes.map.None
-signal map_selected (tool: DataTypes.map) #signal to get what button user pressed on map selection
-func Map_select (map: DataTypes.map) -> void:
-	map_selected.emit(map)
-	selected_map = map
-
 
 func start_game() -> void:
-	#load menu
-	SceneManager.load_main_scene_container()
-	#laod map selection after player presses start
-	SceneManager.load_mapSelect_scene_container()
-	
-	var map: String #string for map chosen from map selection
-	
-	#various failed attempts at fixing this  (dont remove)
-	#match selected_map:
-		#DataTypes.map.Jamaica :
-			#map ="Jamaica"
-		#DataTypes.map.Barbados : 
-			#map = "Barbados"
-		#DataTypes.map.Trinidad :
-			#map = "Trinidad"
-	#print ("",map)
-	#await map_selected # wait for user to input map
-	#assert (selected_map > 0)
-	
-	SceneManager.load_level(map)
-
-
+	pass
 	
 func exit_game() -> void:
 	get_tree().quit()
